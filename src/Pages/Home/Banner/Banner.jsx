b8ab1dcf9146d8738ab3696aee31d0f4f39b8de3
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-05-10 19:35:08 +0600

    banner and slider have been made responsive for all devices

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,18 +5,18 @@ import 'animate.css';
 
 const Banner = () => {
     return (
-        <div className='relative flex justify-center mb-32 h-[675px]'>
+        <div className='relative flex justify-center mb-20 h-[675px]'>
             <img src={bannerPhoto} alt="" className='w-full absolute z-10'/>
-            <div className='w-full h-[675px] absolute z-20 bg-gradient-to-b from-[#724F54]'></div>
-            <div className='absolute z-20 top-20'>
-                <h1 className='text-5xl font-playfair text-white text-center'>Empower Your Choices <br /> Crowdsource Product Insights & Solutions!</h1>
+            <div className='w-full h-[188px] md:h-[385px] lg:h-[675px] absolute z-20 bg-gradient-to-b from-[#724F54]'></div>
+            <div className='absolute z-20 top-8 md:top-20'>
+                <h1 className='text-2xl md:text-4xl lg:text-5xl font-playfair text-white text-center'>Empower Your Choices <br /> Crowdsource Product Insights & Solutions!</h1>
             </div>
-            <div className='absolute z-20 w-[461px] top-[300px] left-[440px] h-[340px] bg-[#431A20] animate__animated animate__fadeInUp text-center p-11'>
-                <h1 className='text-4xl text-white font-playfair'>Welcome to <br /> Reflect Radar</h1>
-                <p className='text-white text-center mt-5'>Join our community to share product concerns, discover alternative solutions, and engage with fellow users for informed choices and better shopping experiences.</p>
-                <div className='flex items-center justify-center gap-7 mt-5'>
-                    <p className='text-white'>Explore More</p>
-                    <Link><button className="btn btn-circle bg-[#CB2903] hover:bg-[#431A20] duration-300 border-none"><FaArrowRightLong className='text-white  text-xl'></FaArrowRightLong></button></Link>
+            <div className='absolute z-20 w-[300px] md:w-[461px] top-[150px] md:top-[250px] lg:top-[300px] left-10 md:left-[160px] lg:left-[440px] h-[150px] md:h-[230px] lg:h-[340px] bg-[#431A20] animate__animated animate__fadeInUp text-center p-3 md:p-7 lg:p-11'>
+                <h1 className='text-2xl md:text-3xl lg:text-4xl text-white font-playfair w-full lg:w-[250px] mx-auto'>Welcome to Reflect Radar</h1>
+                <p className='text-white text-center mt-2 md:mt-5 text-[10px] md:text-base'>Join our community to share product concerns, discover alternative solutions, and engage with fellow users for informed choices and better shopping experiences.</p>
+                <div className='flex items-center justify-center gap-7 mt-2 lg:mt-5'>
+                    <p className='text-white text-[14px] md:text-base'>Explore More</p>
+                    <Link><button className="btn btn-circle bg-[#CB2903] btn-sm md:btn-md hover:bg-[#431A20] duration-300 border-none"><FaArrowRightLong className='text-white text-base md:text-xl'></FaArrowRightLong></button></Link>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -15,51 +15,92 @@ import slide5 from '../../../assets/slider5.jpg';
 const Slider = () => {
 
     return (
-        <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
-            effect="fade"
-            spaceBetween={50}
-            slidesPerView={1}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            navigation
-            loop='true'
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            <SwiperSlide>
-                <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8 border-4 border-red-700 w-full h-[650px]'>
-                    <div className='absolute h-full inset-0 bg-cover bg-center' style={{backgroundImage:`url(${slide1})`}}/>
-                    <div className='absolute inset-0 bg-black opacity-10 group-hover:opacity-50'/>
-                    <div className='relative flex flex-col gap-3'>
-                        <h1>Hello sujana</h1>
-                    </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8 border-4 border-red-700 w-full h-[650px]'>
-                    <div className='absolute h-full inset-0 bg-cover bg-center' style={{backgroundImage:`url(${slide2})`}}/>
-                    <div className='absolute inset-0 bg-black opacity-10 group-hover:opacity-50'/>
-                    <div className='relative flex flex-col gap-3'>
-                        <h1>Hello shaon</h1>
-                    </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8 border-4 border-red-700 w-full h-[650px]'>
-                    <div className='absolute h-full inset-0 bg-cover bg-center' style={{backgroundImage:`url(${slide3})`}}/>
-                    <div className='absolute inset-0 bg-black opacity-10 group-hover:opacity-50'/>
-                    <div className='relative flex flex-col gap-3'>
-                        <h1>Hello sakib</h1>
-                    </div>
-                </div>
-            </SwiperSlide>
-            ...
-        </Swiper>
+        <div>
+            <div className='text-center mb-10'>
+                <h1 className='text-3xl md:text-5xl font-bold font-playfair mb-5'>Why Choose Us</h1>
+                <p className=''>Discover superior solutions effortlessly. Expert advice, community support, and satisfaction guaranteed.</p>
+            </div>
+            <div>
+                <Swiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+                    effect="fade"
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    navigation
+                    loop='true'
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}
+                >
+                    <SwiperSlide>
+                        <div className='flex flex-col gap-4 group relative text-white rounded-xl px-6 py-8  w-full h-[300px] md:h-[650px]'>
+                            <div className='absolute h-full inset-0 bg-cover bg-center w-[90%] mx-auto rounded-xl' style={{ backgroundImage: `url(${slide1})` }} />
+                            <div className='absolute inset-0 bg-gradient-to-r from-black rounded-xl opacity-10 w-[90%] mx-auto group-hover:opacity-50 duration-300' />
+                            <div className='relative flex flex-col gap-3 justify-center'>
+                                <div className='absolute top-6 md:top-48 left-3 md:left-20 w-[300px] md:w-[500px]'>
+                                    <h1 className='text-2xl md:text-5xl font-bold font-playfair mb-4'>See the difference? <br />Find better alternatives text-2xl md:effortlessly.</h1>
+                                    <p>Quickly discern between product quality. Effortlessly discover superior alternatives by comparing damaged goods to pristine options side by side.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8  w-full h-[300px] md:h-[650px]'>
+                            <div className='absolute h-full inset-0 bg-cover bg-center w-[90%] mx-auto rounded-xl' style={{ backgroundImage: `url(${slide2})` }} />
+                            <div className='absolute inset-0 bg-gradient-to-r from-black rounded-xl opacity-10 w-[90%] mx-auto group-hover:opacity-50 duration-300' />
+                            <div className='relative flex flex-col gap-3 justify-center'>
+                                <div className='absolute top-6 md:top-48 left-3 md:left-20 w-[300px] md:w-[500px]'>
+                                    <h1 className='text-2xl md:text-5xl font-bold font-playfair mb-4'>Join a vibrant community of problem-solvers. Your next solution is just a click away.</h1>
+                                    <p>Collaborate with a diverse community. Access solutions and support from a vibrant network of problem-solvers, just a click away.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8 w-full h-[300px] md:h-[650px]'>
+                            <div className='absolute h-full inset-0 bg-cover bg-center w-[90%] mx-auto rounded-xl' style={{ backgroundImage: `url(${slide3})` }} />
+                            <div className='absolute inset-0 bg-gradient-to-r from-black rounded-xl opacity-10 w-[90%] mx-auto group-hover:opacity-50 duration-300' />
+                            <div className='relative flex flex-col gap-3 justify-center'>
+                                <div className='absolute top-6 md:top-48 left-3 md:left-20 w-[300px] md:w-[500px]'>
+                                    <h1 className='text-2xl md:text-5xl font-bold font-playfair mb-4'>Trust the experts. Our platform curates the best advice tailored just for you.</h1>
+                                    <p>Tap into expert insights. Benefit from curated advice tailored to your needs, sourced from verified experts in the field.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8  w-full h-[300px] md:h-[650px]'>
+                            <div className='absolute h-full inset-0 bg-cover bg-center w-[90%] mx-auto rounded-xl' style={{ backgroundImage: `url(${slide4})` }} />
+                            <div className='absolute inset-0 bg-gradient-to-r from-black rounded-xl opacity-10 w-[90%] mx-auto group-hover:opacity-50 duration-300' />
+                            <div className='relative flex flex-col gap-3 justify-center'>
+                                <div className='absolute top-6 md:top-48 left-3 md:left-20 w-[300px] md:w-[500px]'>
+                                    <h1 className='text-2xl md:text-5xl font-bold font-playfair mb-4'>Navigate effortlessly. Our user-friendly interface makes finding solutions a breeze.</h1>
+                                    <p> Intuitive navigation made simple. Seamlessly browse solutions on our platform with our user-friendly interface across multiple devices.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='flex flex-col gap-4 group relative shadow-lg text-white rounded-lg px-6 py-8  w-full h-[300px] md:h-[650px]'>
+                            <div className='absolute h-full inset-0 bg-cover bg-center w-[90%] mx-auto rounded-xl' style={{ backgroundImage: `url(${slide5})` }} />
+                            <div className='absolute inset-0 bg-gradient-to-r from-black rounded-xl opacity-10 w-[90%] mx-auto group-hover:opacity-50 duration-300' />
+                            <div className='relative flex flex-col gap-3 justify-center'>
+                                <div className='absolute top-6 md:top-48 left-3 md:left-20 w-[300px] md:w-[500px]'>
+                                    <h1 className='text-2xl md:text-5xl font-bold font-playfair mb-4'>Satisfaction guaranteed. Find peace of mind with our reliable recommendations.</h1>
+                                    <p>Confidence in your choices. Experience peace of mind with our reliable recommendations, ensuring satisfaction with every purchase.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    ...
+                </Swiper>
+            </div>
+        </div>
     );
 };
 
